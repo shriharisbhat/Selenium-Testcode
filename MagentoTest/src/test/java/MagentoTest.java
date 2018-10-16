e9c6ef186e4248009998ce6aed847892cf5b5a25
Shrihari Bhat <Shrihari Bhat@LAPTOP-J34M233A>
2018-10-16 23:21:16 +0530

    2nd push

--- MagentoTest/src/test/java/MagentoTest.java
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 
@@ -14,7 +14,7 @@ public class MagentoTest {
 	
 	{
 		String url = "https://www.magento.com";
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		
